62e145663670adcaa3d0cb516c237053a7b515d7
kayla <kayla@LAPTOP-QJ90T6UQ.mynetworksettings.com>
2024-01-03 21:34:03 -0500

    still trying things

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -3,8 +3,36 @@ package b;
 public class DemoB {
 
 	public static void main(String[] args) {
+		
+		//first push (first sprint 
 		System.out.println("BBBBBBBBB");
 		System.out.println("BBBBBBBBB");
+		
+		//second 
+		String day = "Monday";
+		String result = " ";
+
+		switch (day) {
+
+		case "Monday":
+		case "Wednesday":
+		case "Friday":
+		case "Sunday":
+			result = "Lecture Day";
+			break;
+		case "Tuesday":
+		case "Thursday":
+			result = "Tutoring Session";
+			break;
+		case "Saterday":
+			result = "Java Fun Day";
+			break;
+		default:
+			result = " Wrong Imput";
+		}
+			System.out.println(result);
+		
+	}
 	}
 
-}
+
